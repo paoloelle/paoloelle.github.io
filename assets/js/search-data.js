f9d756db44c10be6f40a1ec779df2a0e6e2394a6
paoloelle <61319213+paoloelle@users.noreply.github.com>
2024-12-28 09:51:44 +0000

    Deploying to gh-pages from @ paoloelle/paoloelle.github.io@dc50eee7f7a5f8787a81657f594f190c3dce130c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -509,13 +509,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/paolo-leopardi-59941921b", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
