2ae29475e1333abe41afdd55dfe91bd95f864861
paoloelle <61319213+paoloelle@users.noreply.github.com>
2025-01-27 10:15:34 +0000

    Deploying to gh-pages from @ paoloelle/paoloelle.github.io@1b153bdc700197a58e6548a5ac8bcbfa8321af35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,6 +430,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/paololeopardi.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
